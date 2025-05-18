34409630953c098e9553c0a46fe386d19d915447
ilgnerbecheleni <ilgner.gui@gmail.com>
2025-05-18 18:13:11 -0300

    Adicionar arquivos de projeto.

---
+++ Leitura Serial/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace Leitura_Serial
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnConecta = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.value = new System.Windows.Forms.TextBox();
+            this.aGauge1 = new System.Windows.Forms.AGauge();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // btnConecta
+            // 
+            this.btnConecta.Location = new System.Drawing.Point(139, 12);
+            this.btnConecta.Name = "btnConecta";
+            this.btnConecta.Size = new System.Drawing.Size(88, 23);
+            this.btnConecta.TabIndex = 1;
+            this.btnConecta.Text = "Conectar";
+            this.btnConecta.UseVisualStyleBackColor = true;
+            this.btnConecta.Click += new System.EventHandler(this.btnConecta_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Red;
+            this.textBox1.Location = new System.Drawing.Point(12, 71);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.Red;
+            this.textBox2.Location = new System.Drawing.Point(127, 71);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // value
+            // 
+            this.value.Location = new System.Drawing.Point(12, 132);
+            this.value.Name = "value";
+            this.value.Size = new System.Drawing.Size(215, 20);
+            this.value.TabIndex = 4;
+            // 
+            // aGauge1
+            // 
+            this.aGauge1.BaseArcColor = System.Drawing.Color.Gray;
+            this.aGauge1.BaseArcRadius = 80;
+            this.aGauge1.BaseArcStart = 135;
+            this.aGauge1.BaseArcSweep = 270;
+            this.aGauge1.BaseArcWidth = 2;
+            this.aGauge1.Center = new System.Drawing.Point(100, 100);
+            this.aGauge1.Location = new System.Drawing.Point(12, 180);
+            this.aGauge1.MaxValue = 100F;
+            this.aGauge1.MinValue = 0F;
+            this.aGauge1.Name = "aGauge1";
+            this.aGauge1.NeedleColor1 = System.Windows.Forms.AGaugeNeedleColor.Gray;
+            this.aGauge1.NeedleColor2 = System.Drawing.Color.DimGray;
+            this.aGauge1.NeedleRadius = 80;
+            this.aGauge1.NeedleType = System.Windows.Forms.NeedleType.Advance;
+            this.aGauge1.NeedleWidth = 2;
+            this.aGauge1.ScaleLinesInterColor = System.Drawing.Color.Black;
+            this.aGauge1.ScaleLinesInterInnerRadius = 73;
+            this.aGauge1.ScaleLinesInterOuterRadius = 80;
+            this.aGauge1.ScaleLinesInterWidth = 1;
+            this.aGauge1.ScaleLinesMajorColor = System.Drawing.Color.Black;
+            this.aGauge1.ScaleLinesMajorInnerRadius = 70;
+            this.aGauge1.ScaleLinesMajorOuterRadius = 80;
+            this.aGauge1.ScaleLinesMajorStepValue = 50F;
+            this.aGauge1.ScaleLinesMajorWidth = 2;
+            this.aGauge1.ScaleLinesMinorColor = System.Drawing.Color.Gray;
+            this.aGauge1.ScaleLinesMinorInnerRadius = 75;
+            this.aGauge1.ScaleLinesMinorOuterRadius = 80;
+            this.aGauge1.ScaleLinesMinorTicks = 9;
+            this.aGauge1.ScaleLinesMinorWidth = 1;
+            this.aGauge1.ScaleNumbersColor = System.Drawing.Color.Black;
+            this.aGauge1.ScaleNumbersFormat = null;
+            this.aGauge1.ScaleNumbersRadius = 95;
+            this.aGauge1.ScaleNumbersRotation = 0;
+            this.aGauge1.ScaleNumbersStartScaleLine = 0;
+            this.aGauge1.ScaleNumbersStepScaleLines = 1;
+            this.aGauge1.Size = new System.Drawing.Size(215, 186);
+            this.aGauge1.TabIndex = 5;
+            this.aGauge1.Text = "aGauge1";
+            this.aGauge1.Value = 0F;
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 2000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(258, 385);
+            this.Controls.Add(this.aGauge1);
+            this.Controls.Add(this.value);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnConecta);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Leitura Serial";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnConecta;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox value;
+        private System.Windows.Forms.AGauge aGauge1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ Leitura Serial/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Leitura_Serial
+{
+    public partial class Form1 : Form
+    {
+        public string[] portasatuais = new string[0];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            atualizarListaPortas();
+        }
+
+        private void atualizarListaPortas()
+        {
+            string[] portas = SerialPort.GetPortNames();
+
+            if (!portas.SequenceEqual(portasatuais))
+            {
+                portasatuais = portas;
+                comboBox1.Items.Clear();
+                comboBox1.Items.AddRange(portas);
+
+
+                if (portas.Length > 0)
+                {
+                    comboBox1.SelectedIndex = 0;
+                }
+            }
+        }
+
+        private void btnConecta_Click(object sender, EventArgs e)
+        {
+            if (!serialPort1.IsOpen)
+            {
+                if (comboBox1.SelectedItem == null)
+                {
+                    MessageBox.Show("selecione uma porta serial para se conectar!!");
+                    return;
+                }
+
+                try
+                {
+                    serialPort1.PortName = comboBox1.SelectedItem.ToString();
+                    serialPort1.BaudRate = 9600;
+                    serialPort1.Open();
+
+                    btnConecta.Text = "Desconectar";
+                    comboBox1.Enabled = false;
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("erro ao se conectar" + ex.Message);
+                }
+
+
+
+
+            }
+            else
+            {
+                try
+                {
+                    serialPort1.Close();
+                    btnConecta.Text = "Conectar";
+                    comboBox1.Enabled = true;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("erro ao se desconectar" + ex.Message);
+                }
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if(serialPort1.IsOpen){
+                serialPort1.Write("READ");
+                // A:512,D1:1,D2:0
+            }
+        }
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                string resposta = serialPort1.ReadLine();
+                
+              
+                this.Invoke((MethodInvoker)(() => {
+                    Processarresposta(resposta);
+                }));
+
+            }
+            catch
+            {
+
+            }
+        }
+
+        public void Processarresposta( string resposta)
+        {
+            string[] partes = resposta.Split(',');
+            value.Text = resposta;
+            foreach (string parte in partes)
+            {
+                if (parte.StartsWith("A:"))
+                {
+                    string v1 = parte.Substring(2);
+                    if( float.TryParse(v1 , out float val))
+                    {
+                        aGauge1.Value = val;
+                    }
+
+                }else if (parte.StartsWith("D1:"))
+                {
+                    textBox1.BackColor = parte.EndsWith("1")? Color.Green : Color.Red;
+                }
+                else if (parte.StartsWith("D2:"))
+                {
+                    textBox2.BackColor = parte.EndsWith("1") ? Color.Green : Color.Red;
+                }
+
+
+
+
+            }
+
+        }
+
+
+
+    }
+}
